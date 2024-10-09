00b0b2279750e98aa568723800fe273c1f729445
Saimall <113211361+Saimall@users.noreply.github.com>
2024-10-09 23:52:55 +0530

    Add files via upload
    Added Sprin secuirty service layer

---
+++ springsecuirtydemo2/src/main/java/com/example/springsecuirtydemo2/Controller/UserControllers.java
@@ -0,0 +1,66 @@
+package com.example.springsecuirtydemo2.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springsecuirtydemo2.Services.UserService;
+import com.example.springsecuirtydemo2.models.Userrights;
+
+@RestController
+@RequestMapping("/authenticate")
+public class UserControllers {
+	
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	
+	@PostMapping("/register")
+	public Userrights addUserrights(@RequestBody Userrights userrights) {
+		return userService.addUserrights(userrights);
+	}
+	
+	@GetMapping("/getallusers")
+	public List<Userrights> gelallUserrights(){
+		return userService.getallUserrights();
+	}
+	
+	@PostMapping("/validate/user")
+	public String validateuser(@RequestBody Userrights userrights) {
+		
+		System.out.println("users: "+userrights);
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userrights.getPassword(),userrights.getUsername()));
+		if(authentication.isAuthenticated()) {
+			return  userService.getToken(userrights.getUsername());
+					
+		}
+		return null;
+		
+		
+		
+	}
+	
+	
+	@GetMapping("/validate/token")
+	public boolean validatetoken(@RequestParam String token) {
+		return userService.validatetoken(token);
+	}
+	
+
+
+}
---
+++ springsecuirtydemo2/src/main/java/com/example/springsecuirtydemo2/Secuirtyconfig/SecuirtyConfig.java
@@ -0,0 +1,73 @@
+package com.example.springsecuirtydemo2.Secuirtyconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.springsecuirtydemo2.Services.CustomUserDetailsService;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecuirtyConfig {
+	
+	//why autowirig userdtailsservice
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	
+	
+	//.httpbasic(customizer->customizer.withDefault()) -->this is used to The httpBasic(Customizer.withDefaults()) is typically used to configure HTTP Basic authentication for a web application.
+	
+	//.headers(headers->headers.frameoptions(frame->frame.withdefault()))
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		
+		http.csrf(csrf->csrf.disable()).authorizeHttpRequests(request->request.anyRequest().permitAll()).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).headers(header->header.frameOptions(frame->frame.sameOrigin())) ;
+	
+	
+		return http.build();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationmanager( AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+		
+	}
+	
+	
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		
+		daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+		
+		
+		daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+		
+		return daoAuthenticationProvider;
+	}
+
+
+ 
+
+
+
+	
+
+}
